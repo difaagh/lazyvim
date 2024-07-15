b55eac0ecc4d479c7088ae182124edc56117a639
difaagh <dhifa.aghisni@gmail.com>
2024-07-15 08:31:16 +0000

    add function to copy to clipboard with v mode and <leader>y

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- copy to clipboard
+function CopyToDevClipboard()
+    local old_reg = vim.fn.getreg('"')  -- save current register contents
+    vim.cmd([[normal! gv]])  -- select current visual area
+    local selection = vim.fn.getreg('"')  -- get content of selection
+    os.execute('echo ' .. vim.fn.shellescape(selection) .. ' > /dev/clipboard')  -- write selection to /dev/clipboard
+    vim.fn.setreg('"', old_reg)  -- restore original register contents
+end
+
+-- Map the function <leader>y in normal mode
+vim.api.nvim_set_keymap('n', '<leader>y', ':lua CopyToDevClipboard()<CR>', { noremap = true, silent = true })
