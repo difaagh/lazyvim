90f0637f3ccf65107362bf4bd594b82f5d71cef3
difaagh <dhifa.aghisni@gmail.com>
2024-07-20 17:28:25 +0700

    update ruby.lua plugin
    - add sorbet lsp
    - add slim template syntax highlighting by
    [slim-template/vim-slim](https://github.com/slim-template/vim-slim)

--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -1,36 +1,28 @@
 local ruby_version = vim.fn.system("ruby -v"):match("(%d+%.%d+%.%d+)")
 local ruby_minimal_version = "3.0.0"
-local solargraph = {}
 local rubocop = {}
 
 -- only override necessary settings
 if ruby_version then
-  -- use solargraph and rubocop system
-  if ruby_version <= ruby_minimal_version then
-    solargraph = {
-      mason = false,
-      formatting = false,
-    }
-    -- old ruby version typically doesn't support higher rubocop version for example
-    -- ruby version 2.6.10 use rubocop 1.15.0 version. this version is not support --lsp or --server argument
+  -- custom rubocop lsp setting
+  if ruby_version < ruby_minimal_version then
+    -- old ruby version typically doesn't support higher rubocop version
+    -- for example ruby version 2.6.10 use rubocop 1.15.0 version. this version is not support --lsp or --server argument
     -- use rubocop cmd without the argument
     rubocop = {
       mason = false,
       cmd = { "rubocop" },
     }
-    -- solargraph and rubocop use mason installation
   else
-    solargraph = {
-      mason = true,
-      formatting = false,
-    }
+    -- use default cmd provided by nvim-lspconfig
     rubocop = {
-      mason = true,
+      mason = false,
     }
   end
 end
 
 return {
+  -- ruby syntax highlighting
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
@@ -39,13 +31,30 @@ return {
       })
     end,
   },
+  -- slim template syntax highlighting
+  {
+    "slim-template/vim-slim",
+  },
 
+  -- ruby language server protocol
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         rubocop = rubocop,
-        solargraph = solargraph,
+        solargraph = {
+          mason = false,
+          formatting = false,
+        },
+        sorbet = {
+          -- use sorbet local package
+          -- TODO: check wether need to use global or local package
+          cmd = { "bundle", "exec", "srb", "tc", "--lsp", "--typed=true" },
+          mason = false,
+          root_dir = function(in_current_path)
+            return require("lspconfig.util").root_pattern("Gemfile")(in_current_path)
+          end,
+        },
       },
     },
   },
